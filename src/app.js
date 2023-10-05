e9b74b36739bd22f47e340298f9fab042c0f7770
Thepbordin Jaiinsom <thepbordinjaiinsom@gmail.com>
2023-10-05 23:58:36 +0700

    more init

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+import roomRoute from "./routes/roomRoute.js";
+
+const app = express();
+
+// body-parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// allow request from other origin (Frontend which is at different port)
+app.use(cors());
+
+// use routes
+app.use("/rooms", roomRoute);
+
+export default app;
